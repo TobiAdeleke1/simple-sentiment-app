5d45ecff2460581310ff06d24c97a5aa9604d300
Tobi Adeleke <tobiaadeleke@gmail.com>
2023-05-08 22:56:57 +0100

    add selenium unittest setup and teardown

--- sentiment_app/tests/test_index_web.py
+++ sentiment_app/tests/test_index_web.py
@@ -1,2 +1,18 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class WebTests(unittest.TestCase):
+    def setUp(self) -> None:
+        # self.driver = webdriver.Chrome(executable_path=r'C:/Users/damol/OneDrive/Life_After_UNI/Personal_Study/MARCH_APRIL/TDD/chromedriver')
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.driver.get('http://localhost:5000')
+
+    def test_sentiment_app_name(self) ->None:
+        driver = self.driver
+        self.assertIn("Sentiment", driver.title)
+
+    def tearDown(self) -> None:
+        self.driver.close()
