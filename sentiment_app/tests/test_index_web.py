2b1c731ba695d5c22be8312f8c39ff46c5752f58
Tobi Adeleke <tobiaadeleke@gmail.com>
2023-05-14 01:44:30 +0100

    adding frontend selenium tests

--- sentiment_app/tests/test_index_web.py
+++ sentiment_app/tests/test_index_web.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -11,8 +12,26 @@ class WebTests(unittest.TestCase):
         self.driver.get('http://localhost:5000')
 
     def test_sentiment_app_name(self) ->None:
-        driver = self.driver
-        self.assertIn("Sentiment", driver.title)
+        selenium_chrome = self.driver
+        self.assertIn("Sentiment", selenium_chrome.title)
+    
+    def test_page_heading_is_named_sentiment_analysis(self):
+        heading = self._find('heading').text
+        self.assertEqual("Sentiment Analysis", heading)
+
+    def test_page_has_input_text(self):
+        input_text = self._find('large-text').text
+        self.assertIsNotNone(input_text)
+
+    def test_page_has_submit_button(self):
+        submit_button = self._find('submit-button')
+        self.assertIsNotNone(submit_button)
+    
+    def _find(self,val):
+        data_attribute = f'[test-id="{val}"]'
+        element = self.driver.find_element(By.CSS_SELECTOR, data_attribute)  
+        return element
+
 
     def tearDown(self) -> None:
         self.driver.close()
